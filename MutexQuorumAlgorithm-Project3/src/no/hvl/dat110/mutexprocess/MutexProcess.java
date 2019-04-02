e1087fde10aa6517146433e195ab4e8f64720d52
h571560 <kristoffer-hp@hotmail.com>
2019-04-02 09:59:37 +0200

    lamda uttrykk for majorityAcknowledgement metode

--- MutexQuorumAlgorithm-Project3/src/no/hvl/dat110/mutexprocess/MutexProcess.java
+++ MutexQuorumAlgorithm-Project3/src/no/hvl/dat110/mutexprocess/MutexProcess.java
@@ -185,11 +185,8 @@ public class MutexProcess extends UnicastRemoteObject implements ProcessInterfac
 		// count the number of yes (i.e. where message.isAcknowledged = true)
 		// check if it is the majority or not
 		// return the decision (true or false)
-
-				
-				
-				
-		return false;			// change this to the result of the vote
+		counter = (int) queueACK.stream().filter(x -> x.isAcknowledged()).count();
+		return counter >= quorum;// change this to the result of the vote
 	}
 
 		
